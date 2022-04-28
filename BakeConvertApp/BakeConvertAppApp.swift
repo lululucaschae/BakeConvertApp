afca9ce6062cb5f0ee7b689ee6f5dddc9546148d
Lucas Chae <hi@lucaschae.com>
2022-04-28 10:48:43 +0900

    Initial Commit

---
+++ BakeConvertApp/BakeConvertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeConvertAppApp.swift
+//  BakeConvertApp
+//
+//  Created by Lucas Chae on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BakeConvertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
