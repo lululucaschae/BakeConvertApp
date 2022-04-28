20541a1fa5b6ebffc1ba2740603e87cd73b839e1
Lucas Chae <hi@lucaschae.com>
2022-04-28 18:34:14 +0900

    WeightConversion Function Definition

--- BakeConvertApp/ContentView.swift
+++ BakeConvertApp/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
+
+    @State private var selectedPage = 1
     @State private var weightInputSample = 0.0
     @State private var weightInputUnit = "pounds (lb)"
     @State private var weightOutputUnit = "pounds (lb)"
@@ -17,101 +18,160 @@ struct ContentView: View {
     let weightUnits = ["pounds (lb)", "ounces (oz)", "grams (g)", "kilograms (kg)"]
     let defaultMeasurements = Array(stride(from: 0.00, through: 100.0, by: 0.5))
     
+    func WeightConversion(unitWeight: Measurement<UnitMass>, thisUnit: String) -> Double {
+        switch thisUnit {
+        case "pounds (lb)":
+            return unitWeight.converted(to: .pounds).value
+        case "ounces (oz)":
+            return unitWeight.converted(to: .ounces).value
+        case "grams (g)" :
+            return unitWeight.converted(to: .grams).value
+        case "kilograms (kg)" :
+            return unitWeight.converted(to: .kilograms).value
+        default:
+            return 0
+        }
+    }
     
     var convertedUnit: Double {
         if weightInputUnit == "pounds (lb)" {
+            let unitWeight = Measurement(value: weightInputSample, unit:UnitMass.pounds)
+            
             switch weightOutputUnit {
             case "pounds (lb)":
-                return weightInputSample * 1.0
+                return unitWeight.converted(to: .pounds).value
             case "ounces (oz)":
-                return weightInputSample * 16
+                return unitWeight.converted(to: .ounces).value
             case "grams (g)" :
-                return weightInputSample * 453.59
+                return unitWeight.converted(to: .grams).value
             case "kilograms (kg)" :
-                return weightInputSample * 0.454
+                return unitWeight.converted(to: .kilograms).value
             default:
                 return weightInputSample
             }
         }
         else if weightInputUnit == "ounces (oz)" {
+            let unitWeight = Measurement(value: weightInputSample, unit:UnitMass.ounces)
             switch weightOutputUnit {
             case "pounds (lb)":
-                return weightInputSample * 0.0625
+                return unitWeight.converted(to: .pounds).value
             case "ounces (oz)":
-                return weightInputSample * 1
+                return unitWeight.converted(to: .ounces).value
             case "grams (g)" :
-                return weightInputSample * 28.3495
+                return unitWeight.converted(to: .grams).value
             case "kilograms (kg)" :
-                return weightInputSample * 0.00283495
+                return unitWeight.converted(to: .kilograms).value
             default:
                 return weightInputSample
             }
         }
         else if weightInputUnit == "grams (g)" {
+            let unitWeight = Measurement(value: weightInputSample, unit:UnitMass.grams)
             switch weightOutputUnit {
             case "pounds (lb)":
-                return weightInputSample * 0.00220462
+                return unitWeight.converted(to: .pounds).value
             case "ounces (oz)":
-                return weightInputSample * 0.035274
+                return unitWeight.converted(to: .ounces).value
             case "grams (g)" :
-                return weightInputSample * 1
+                return unitWeight.converted(to: .grams).value
             case "kilograms (kg)" :
-                return weightInputSample * 0.001
+                return unitWeight.converted(to: .kilograms).value
             default:
                 return weightInputSample
             }
         }
         else {
+            let unitWeight = Measurement(value: weightInputSample, unit:UnitMass.grams)
             switch weightOutputUnit {
             case "pounds (lb)":
-                return weightInputSample * 2.20462
+                return unitWeight.converted(to: .pounds).value
             case "ounces (oz)":
-                return weightInputSample * 35.274
+                return unitWeight.converted(to: .ounces).value
             case "grams (g)" :
-                return weightInputSample * 1000
+                return unitWeight.converted(to: .grams).value
             case "kilograms (kg)" :
-                return weightInputSample * 1
+                return unitWeight.converted(to: .kilograms).value
             default:
                 return weightInputSample
             }
         }
     }
     
+
     
     
     var body: some View {
         NavigationView {
             
-            Form {
-                Section {
-                    Picker("Input Value", selection: $weightInputSample) {
-                        ForEach(defaultMeasurements, id: \.self) {unit in
-                            Text(unit, format: .number)
+            TabView(selection: $selectedPage) {
+                Form {
+                    Section {
+                        Picker("Input Value", selection: $weightInputSample) {
+                            ForEach(defaultMeasurements, id: \.self) {unit in
+                                Text(unit, format: .number)
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        Picker("Input Unit", selection: $weightInputUnit) {
+                            ForEach(weightUnits, id: \.self) {unit in
+                                Text(unit)
+                            }
+                        }
+                        
+                        Picker("Output Unit", selection: $weightOutputUnit) {
+                            ForEach(weightUnits, id: \.self) {unit in
+                                Text(unit)
+                            }
                         }
                     }
-                    .pickerStyle(.wheel)
-                    Picker("Input Unit", selection: $weightInputUnit) {
-                        ForEach(weightUnits, id: \.self) {unit in
-                            Text(unit)
-                        }
+                    Section {
+                        Text("\(convertedUnit, specifier: "%.2f")")
+                    } header: {
+                        Text("\(weightInputSample, specifier: "%.2f") \(weightInputUnit) in \(weightOutputUnit) is")
                     }
-                    Picker("Output Unit", selection: $weightOutputUnit) {
-                        ForEach(weightUnits, id: \.self) {unit in
-                            Text(unit)
-                        }
-                    }
+                    
                 }
-                Section {
-                    Text("\(convertedUnit, specifier: "%.2f")")
-                } header: {
-                    Text("Input weight of \(weightInputSample, specifier: "%.2f") in \(weightOutputUnit) is")
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("Weight")
+                }.tag(1)
+                Form {
+                    Section {
+                        Picker("Input Value", selection: $weightInputSample) {
+                            ForEach(defaultMeasurements, id: \.self) {unit in
+                                Text(unit, format: .number)
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        Picker("Input Unit", selection: $weightInputUnit) {
+                            ForEach(weightUnits, id: \.self) {unit in
+                                Text(unit)
+                            }
+                        }
+                        Picker("Output Unit", selection: $weightOutputUnit) {
+                            ForEach(weightUnits, id: \.self) {unit in
+                                Text(unit)
+                            }
+                        }
+                    }
+                    Section {
+                        Text("\(convertedUnit, specifier: "%.2f")")
+                    } header: {
+                        Text("Input weight of \(weightInputSample, specifier: "%.2f") in \(weightOutputUnit) is")
+                    }
+                    
                 }
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("Volume")
+                }.tag(2)
                 
             }
-            .navigationTitle("BakeCovertApp")
+            .navigationTitle(selectedPage == 1 ? "Weight" : "Volume")
 
              
         }
+    
     }
 }
 
